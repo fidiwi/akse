ab7f000b89a9381ab865a2deb1396237facf3e21
fidiwi <friedrichleowinkler@gmx.de>
2020-11-03 08:55:58 +0100

    First Commit

---
+++ AKsE.py
@@ -0,0 +1,154 @@
+from operator import itemgetter
+import json
+import sys
+import regex
+
+#re Multiline comments
+#\/\*(.*?)\*\/
+#\/\*([\S\s]*?)\*\/
+
+#re Singleline comments
+#\/\/(.*?)\n
+config_path = "config.json"
+cpp_path = "input.cpp"
+
+pattern_if = r"if(\s*?)\((.*?)\)(\s*?)\{"
+pattern_else = r"(?<=if\s*\((.*?)\)\s*\{([\s\S]*?)\}(\s*?))else\s*\{"
+pattern_elif = r"(?<=if\s*\((.*?)\)\s*\{([\s\S]*?)\}(\s*?))else if\s*\{"
+
+#CHANGE: "Do-Schleife" aus REQs entfernen
+pattern_for = r"for(\s*?)\((.*?)\)(\s*?)\{"
+pattern_while = r"while(\s*?)\((.*?)\)(\s*?)\{"
+pattern_do_while = r"do\s*\{(?=([\s\S]*?)\}\s*while\s*\(([\s\S]*?)\);)"
+
+
+pattern_switch = r"switch(\s*?)\((.*?)\)(\s*?)\{"
+pattern_switch_case_default = r"switch(\s*?)\((.*?)\)(\s*?)\{\s*(case\s*(.*?):(.*?);)+\s*default:(.*?);\s*\}"
+pattern_case = r"case\s*(.*?):((?=(.*?);\s*default:(.*?);+\s*\})|(?=(.*?);\s*case(.*?):(.*?);))"
+pattern_default = r"default:(?=(.*?);+\s*\})"
+
+
+
+patterns = {
+    1: {"a": pattern_if,
+        "b": pattern_else,
+        "c": pattern_elif},
+    2: {"a": pattern_for,
+        "b": pattern_while,
+        "c": pattern_do_while},
+    3: {"a": pattern_switch,
+        "b": pattern_case,
+        "c": pattern_default}}
+
+class Config:
+
+    def readConfig(self):
+        with open(config_path) as json_file:
+            try:
+                data = json.load(json_file)
+                self.detect = data["detect"]
+                
+            except:
+                #2.2.2.5_Konfigurationsdatei#REAL
+                sys.exit("[Error] Could not load JSON File")
+
+class Analysis:
+
+    def __init__(self, detect):
+        self.detect = detect
+    
+    
+    def readCPP(self):
+        with open(cpp_path, "r") as cpp_file:
+            self.cpp_content = cpp_file.read()
+        
+    
+    def detectCommentedCode(self):
+        
+        #For /* XYZ */ structures
+        pattern1 = regex.compile(r"\/\*(.*?)\*\/", regex.DOTALL) 
+        result1 = regex.finditer(pattern1, self.cpp_content)
+
+        #For //XYZ structures
+        pattern2 = regex.compile(r"\/\/(.*?)\n", regex.DOTALL) 
+        result2 = regex.finditer(pattern2, self.cpp_content)
+
+        self.list_commented = []
+        for match in result1:
+            self.list_commented.append(match.span())
+
+        for match in result2:
+            self.list_commented.append(match.span())
+
+        #print(list_commented)
+    
+    def detectControlStructures(self):
+        results = []
+        for group in self.detect:
+            if group == 1:
+                for i in ("a", "b", "c"):
+                    pattern = regex.compile(patterns[1][i], regex.DOTALL)
+                    result = regex.finditer(pattern, self.cpp_content)
+                    results.append(result)
+            elif group == 2:
+                for i in ("a", "b", "c"):
+                    pattern = regex.compile(patterns[2][i], regex.DOTALL)
+                    result = regex.finditer(pattern, self.cpp_content)
+                    results.append(result)
+            elif group == 3:
+                for i in ("a", "b", "c"):
+                    pattern = regex.compile(patterns[3][i], regex.DOTALL)
+                    result = regex.finditer(pattern, self.cpp_content)
+                    results.append(result)
+            else:
+                sys.exit(str("[Error] Group '" + str(group) + "' requested in config.json does not exist."))
+        
+        return results
+
+    def validateDetection(self, detected_data):
+        content_edited = self.cpp_content
+        result_list = []
+
+        for iterator in detected_data:
+            for item in iterator:
+                result_list.append(item.span())
+
+        #2.2.4.1_Fehler#REAL
+        if(len(result_list) == 0): sys.exit("[Error] None of the requested control structres were detected")
+        #Sort list items by their ending position AND reverse it.
+        reversed_list = sorted(result_list, key=itemgetter(1), reverse=True)
+        for match in reversed_list:
+            start = match[0]
+            insideComments = False
+            
+            #Check if Statement is within comments
+            for comments in self.list_commented:
+                if comments[0] <= start <= comments[1]:
+                    insideComments = True
+                    break
+            
+            if (not insideComments):
+                end = match[1]
+                content_edited = content_edited[:end] + "/*X*/" + content_edited[end:]
+        print(content_edited)
+        return content_edited
+            
+
+config = Config()
+config.readConfig()
+
+detect = config.detect
+
+#If length of "detect" is 0
+#2.2.4.2_Fehler#REAL
+if not len(detect):
+    sys.exit("[Info] Program closed - No control structure requested in JSON File")
+
+analysis = Analysis(detect)
+analysis.readCPP()
+analysis.detectCommentedCode()
+results = analysis.detectControlStructures()
+cpp_commented = analysis.validateDetection(results)
+with open("output.cpp", "w") as file:
+    file.truncate()
+    file.write(cpp_commented)
---
+++ output.cpp
@@ -0,0 +1,255 @@
+/**
+@file       AccComposer.cpp
+
+@brief      This class implements the composer for ACC connections.
+
+@author     kim  Minhwan Kim  (23880-604) <minhwan.kim@ese.de>
+
+@par        Verifikation:
+
+
+@par        Validierung:
+
+
+@par        Code-Review:
+            krm Mandy Poetzsch <mandy.poetzsch@ese.de>
+
+@par        Code-Check:
+            Codechecker Version auf Signatur der Quelle
+
+@note       COPYRIGHT (C)2020 SIEMENS MOBILITY GMBH - ALL RIGHTS RESERVED - INTERNAL USE
+*/
+
+/*
+=== History ====================================================================
+@since 20201022 kim [draft]
+- Modified AccComposer::generateDataTelegrams()
+@since 20201015 krm [released]
+- Checked rework, no remarks
+@since 20201012 kim [review]
+- Responded review
+@since 20201009 krm [review]
+- Review with remarks 
+if (true){
+    doSomething();
+}
+@since 20200725 kim [darft]
+- Added the copy constructor and copy assigment operator
+- Modified the generateStatusTelegrams() and generateDataTelegrams()
+@since 20200509 bow [draft]
+- Initial version
+*/
+
+#if 0
+extern const char SOURCEQUELLID_needed_for_xlogger[] = "@(#)>";
+#endif
+
+// This projects uses no operator replacement macros.
+// BEGIN SUPPRESS 3.1.2
+
+#include "AccComposer.h"
+#include "SafetyOrientedReactor.h"
+
+/**
+ * @brief Information identifier of data telegram.
+ */
+static const WORD cInformationIdentifierData = 1;
+
+/**
+ * @brief Information identifier of status telegram.
+ */
+static const WORD cInformationIdentifierStatus = 2;
+
+/**
+ * @brief Status information of "connection OK".
+ */
+static const BYTE cStatusInformationConnectionOk = 1;
+
+/**
+ * @brief Status information of "connection failed".
+ */
+static const BYTE cStatusInformationConnectionFailed = 2;
+
+// Code_Inspector does not know that destructors cannot be qualified.
+AccComposer::~AccComposer()   // SUPPRESS 8.5.1
+{
+    /*!
+     <SMLSISWA>[ERROR AccComposer::~AccComposer()]:
+     | The destructor is not allowed.
+     | The system will be shutdown.
+     */
+    xlog(1670396960, 0, 0, NULLPTR);
+    pSafetyOrientedReactor->shutdown();
+}
+switch (expression)
+{/*X*/
+case 1:/*X*/break;
+
+default:/*X*/
+    break;
+}
+
+AccComposer::AccComposer(const AccComposer& other) :
+    mpConnectionStates(other.mpConnectionStates), mrElements(other.mrElements),
+    mInterfaceVersionIdentifier(other.mInterfaceVersionIdentifier)
+{
+    /*!
+     <SMLSISWA>[ERROR AccComposer::AccComposer()]:
+     | Internal error!
+     | Cannot use this method because the copy constructor is prohibited.
+     | The system will be shutdown!
+     */
+    xlog(1670388992, 0, 0, NULLPTR);
+    pSafetyOrientedReactor->shutdown();
+}
+
+const AccComposer& AccComposer::operator=(const AccComposer& other) const
+{
+    if (this != &other)
+    {/*X*/
+        /*!
+         <SMLSISWA>[ERROR AccComposer::operator=()]:
+         | Internal error!
+         | Cannot use this method because the assignment operator is prohibited.
+         | The system will be shutdown!
+         */
+        xlog(1670388993, 0, 0, NULLPTR);
+        pSafetyOrientedReactor->shutdown();
+    }
+    return *this;
+}
+
+void AccComposer::setup()
+{
+    // The allocation is already checked by MemoryAllocator.
+    AccComposerConnectionStates *const pConnectionStates =
+        new AccComposerConnectionStates;   // SUPPRESS 10.1.10
+    for (AccComposerElements::Iterator iterator = mrElements.begin();
+        iterator != mrElements.end(); ++iterator)
+    {/*X*/
+        if (pConnectionStates->find(iterator.key()) ==
+            pConnectionStates->end())
+        {/*X*/
+            (*pConnectionStates)[iterator.key()] =
+                iterator.value()->getDispatchingConnection()->
+                    getConnectionState();
+        }
+    }
+    mpConnectionStates = pConnectionStates;
+}
+
+void AccComposer::markElementsAsComposed(const BaseConnection& rConnection)
+{
+    for (AccComposerElements::Iterator iterator = mrElements.begin();
+        iterator != mrElements.end(); ++iterator)
+    {/*X*/
+        iterator.value()->markAsComposed(rConnection);
+    }
+}
+
+bool AccComposer::isAnyConnectionStateConnected() const
+{
+    bool res = false;
+    if (mpConnectionStates != NULLPTR)
+    {/*X*/
+        for (AccComposerConnectionStates::Iterator iterator =
+            mpConnectionStates->begin();
+            iterator != mpConnectionStates->end(); ++iterator)
+        {/*X*/
+            if (iterator.value()->getState() == ConnectionState::nConnected)
+            {/*X*/
+                res = true;
+                // This break improves efficiency.
+                break;   // SUPPRESS 7.2.4
+            }
+        }
+    }
+    return res;
+}
+
+void AccComposer::generateStatusTelegrams(AbstractSender& rSender,
+    bool areAllNeeded)
+{
+    if (mpConnectionStates != NULLPTR)
+    {/*X*/
+        const Timer cNow;
+        for (AccComposerConnectionStates::Iterator iterator =
+            mpConnectionStates->begin();
+            iterator != mpConnectionStates->end(); ++iterator)
+        {/*X*/
+            const ConnectionState* const connectionState = iterator.value();
+            // It should change the flag of changed at first, although
+            // areAllNeeded is true.
+            if (connectionState->isChanged() || areAllNeeded)
+            {/*X*/
+                StatusTelegram telegram = {};
+                telegram.informationIdentifier = cInformationIdentifierStatus;
+                telegram.elementNumber = iterator.key();
+                //@rev[+] 20201008 krm Please avoid using things like: ==
+                //                     ConnectionState::nConnected) ?
+                //                     It is not easy to understand and it is
+                //                     problematic for for coverage measurement.
+                //                     SIL4 code shall be as easily readable as
+                //                     possible.
+                //     -> 20201012 kim Modified.
+                //     -> 20201015 krm Ok.
+                if (connectionState->getState() == ConnectionState::nConnected)
+                {/*X*/
+                    telegram.statusInformation = cStatusInformationConnectionOk;
+                }
+                else
+                {/*X*/
+                    telegram.statusInformation =
+                        cStatusInformationConnectionFailed;
+                }
+
+                // The cast is safe because it can be converted to the type of
+                // the smallest element of it and it is expected.
+                rSender.sendTelegram(reinterpret_cast<const BYTE*>(&telegram),   // SUPPRESS 5.5.8b
+                    sizeof telegram, cNow);
+            }
+        }
+    }
+}
+
+void AccComposer::generateDataTelegrams(AbstractSender& rSender,
+    bool areAllNeeded)
+{
+    const Timer cNow;
+    for (AccComposerElements::Iterator iterator = mrElements.begin();
+        iterator != mrElements.end(); ++iterator)
+    {/*X*/
+        BaseElement* const element = iterator.value();
+        if (areAllNeeded || element->isChanged())
+        {/*X*/
+            // Code_Inspector does not know this kind of well-defined
+            // initialization.
+            DataTelegram telegram = {};   // SUPPRESS 5.1.7
+
+            telegram.informationIdentifier = cInformationIdentifierData;
+            telegram.elementNumber = iterator.key();
+            telegram.interfaceVersion = mInterfaceVersionIdentifier;
+            memsetX(telegram.payload, 0, sizeof telegram.payload);
+
+            if ((element->isChanged() == false) || areAllNeeded)
+            {/*X*/
+                element->encodeToAcc(telegram.payload);
+                // The cast is safe because it can be converted to the type of
+                // the smallest element of it and it is expected.
+                rSender.sendTelegram(reinterpret_cast<const BYTE*>(&telegram),   // SUPPRESS 5.5.8b
+                    sizeof telegram, cNow);
+            }
+            else
+            {/*X*/
+                const Timer& receivingTime =
+                    element->encodeToAcc(telegram.payload);
+                // The cast is safe because it can be converted to the type of
+                // the smallest element of it and it is expected.
+                rSender.sendTelegram(reinterpret_cast<const BYTE*>(&telegram),   // SUPPRESS 5.5.8b
+                    sizeof telegram, receivingTime);
+            }
+        }
+    }
+}
+
+// END SUPPRESS 3.1.2
